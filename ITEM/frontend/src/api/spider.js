033c58c1f0a5c5efb9cf4850633abb4c4dd92e5a
qiaolann <1662652109@qq.com>
2026-01-10 23:57:32 +0800

    frontend1

--- ITEM/frontend/src/api/spider.js
+++
@@ -1,6 +0,0 @@
-// src/api/spider.js
-import http from "./http";
-
-export const runSpider = async () => (await http.post("/api/spider/run")).data;
-export const spiderStatus = async (taskId) => (await http.get(`/api/spider/status/${taskId}`)).data;
-export const spiderResult = async (taskId) => (await http.get(`/api/spider/result/${taskId}`)).data;
